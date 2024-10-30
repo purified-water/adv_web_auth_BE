6be3076ddd0d21351959451f6897755e348c0164
“purified-water” <julianng1110@gmail.com>
2024-10-30 20:52:13 +0700

    add logging FE

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ async function bootstrap() {
   // Use cors
   app.enableCors({
     origin: process.env.REACT_APP_API_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  console.log("FE: ", process.env.REACT_APP_API_URL);
   await app.listen(process.env.PORT ?? 3031);
 }
 bootstrap();
